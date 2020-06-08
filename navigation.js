e022f6189fffa3400e2a336ea82d52dd27239eb2
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-06-08 14:01:36 +0200

    Good directions

--- navigation.js
+++ navigation.js
@@ -3,20 +3,15 @@
 let navigate = (start, end) => {
     const map = document.querySelectorAll(".square"); //same is made in script file
 let n=50
-let startRow = start/n;
-let endRow = end/n;
-let coll = endRow-end/n;
+let startRow = Math.floor(start/n);
+let endRow = Math.floor(end/n);
+let startColl = Math.floor(start%n);
+let endColl = Math.floor(end%n);
+let coll = endColl-startColl;
 let row = startRow-endRow;
-//    0000-0049
-//    0050-0099
-//    0100-0149
-//    ...            50x50 n*n
-//    2450-2499 
     
     document.getElementById("result").innerHTML =
     `GPS: ${start}/${end}
-    start row: ${startRow}
-    end row: ${endRow}
     horizontal: ${coll}
     vertical: ${row}`; //Note! innerHTML makse all file parse again
     
