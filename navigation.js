e5b5f8e8c5b727897568d2c569ffce76d1080759
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-06-08 14:26:07 +0200

    Route squares array
    Track of squeres array and let for const change.

--- navigation.js
+++ navigation.js
@@ -2,18 +2,25 @@
 
 let navigate = (start, end) => {
     const map = document.querySelectorAll(".square"); //same is made in script file
-let n=50
-let startRow = Math.floor(start/n);
-let endRow = Math.floor(end/n);
-let startColl = Math.floor(start%n);
-let endColl = Math.floor(end%n);
-let coll = endColl-startColl;
-let row = startRow-endRow;
+const n=50
+const startRow = Math.floor(start/n);
+const endRow = Math.floor(end/n);
+const startColl = Math.floor(start%n);
+const endColl = Math.floor(end%n);
+const coll = endColl-startColl;
+const row = startRow-endRow;
     
     document.getElementById("result").innerHTML =
     `GPS: ${start}/${end}
     horizontal: ${coll}
     vertical: ${row}`; //Note! innerHTML makse all file parse again
     
+let route = [];
 
+    for(let i = 0; i<Math.abs(coll); i++){
+        route.push(coll>0 ? ++start : --start) //To get the right direction
+    }
+    for(let i = 1; i<Math.abs(row)+1; i++){
+        route.push(row>0 ? start-n*i : start+n*i) //To get the right direction
+    }
 }
